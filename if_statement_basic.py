e3b30bf1d9d2357cc9e1ef710c851d246b9da61a
kentzy1313 <sebastiantyron_bsit@plmun.edu.ph>
2025-02-15 12:55:43 +0800

    Submitting Python Week 04 - Laboratory # 08

---
+++ if_statement_basic.py
@@ -0,0 +1,13 @@
+# Iver John R Monroy
+# ITELEC2
+# Laboratory #08 – Guided Coding Exercise:
+# Variables, Literals, and Case-Sensitivity in Python (with Naming Conventions)
+
+def main():
+
+    temperature = 35  # Or any other number
+    if temperature > 30:
+        print("It's a hot day!")  # Indented 4 spaces
+
+if __name__ == "__main__":
+    main()
